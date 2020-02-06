8b8607fc029ef545f449f18759b040651f03e802
lurui <luruigx08@163.com>
2020-02-06 22:49:23 +0800

    feat(*): sum of two num
    sum of two num
    
    Closes #001

--- src/com/snow/leetcode/Lesson1.java
+++ src/com/snow/leetcode/Lesson1.java
@@ -1,11 +1,49 @@
 package com.snow.leetcode;
 
+import java.util.HashMap;
+
 /**
  * @author lurui
  * @Package com.snow.leetcode
  * @ClassName Lesson1
- * @Description TODO 类描述
+ * @Description lesson1
  * @Date 2020/2/6 10:18 下午
  */
 public class Lesson1 {
+
+//    给定一个整数数组 nums 和一个目标值 target，请你在该数组中找出和为目标值的那 两个 整数，
+//    并返回他们的数组下标。
+//    你可以假设每种输入只会对应一个答案。但是，你不能重复利用这个数组中同样的元素。
+//
+//    给定 nums = [2, 7, 11, 15], target = 9
+//    因为 nums[0] + nums[1] = 2 + 7 = 9
+//    所以返回 [0, 1]
+
+    public static void main(String[] args) {
+        int[] nums = {2, 7, 11, 15};
+        int target = 22;
+        int[] result = twoSum(nums, target);
+        for (int i = 0; i < result.length; i++) {
+            System.out.println(result[i]);
+        }
+    }
+
+    public static int[] twoSum(int[] nums, int target) {
+        /**
+         * 哈希表作为缓存，在算法优化中，可以起到临时存储并检索数据的作用
+         */
+        HashMap<Integer, Integer> map = new HashMap<>();
+        int[] res = new int[2];
+        for (int i = 0; i < nums.length; i++) {
+            int dif = target - nums[i];
+            if (map.get(dif) != null) {
+                res[0] = map.get(dif);
+                res[1] = i;
+                return res;
+            }
+            map.put(nums[i], i);
+        }
+        return res;
+    }
+
 }
